530a00b455407d4fcac50f421c5e1b8f7298b6ca
leolazz <leolazzarini101@gmail.com>
2022-02-24 16:06:36 -0800

    updated user entity, added user.decorator

--- src/dal/entity/user.entity.ts
+++ src/dal/entity/user.entity.ts
@@ -14,7 +14,6 @@ export class User {
   @Column()
   public email: string;
 
-  @Field(() => String)
   @Column()
   public password: string;
 
---
+++ src/users/user.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { Payload } from '../auth/payload.dto';
+
+export const UserId = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    const payload = ctx.getContext().req.user as Payload;
+    return payload.userId;
+  },
+);
