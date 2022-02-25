da1c483f7fb40a687f99f8405ec78d36e0a2fa3a
leolazz <leolazzarini101@gmail.com>
2022-02-24 16:08:06 -0800

    added user resolver & updated user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UseGuards } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { GqlJwtAuthGuard } from '../auth/gql-jwt-auth.guard';
 import { User } from '../dal/entity/user.entity';
+
 @Injectable()
 export class UsersService {
   constructor(
@@ -9,6 +11,9 @@ export class UsersService {
     private readonly usersRepository: Repository<User>,
   ) {}
 
+  public async getUser(userId: any) {
+    return await this.usersRepository.findOne({ id: userId });
+  }
   public async findOne(email: string) {
     return await this.usersRepository.findOne({ email });
   }
